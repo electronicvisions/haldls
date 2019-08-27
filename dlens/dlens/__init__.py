39525744a2e9999a0a38d5a57e97e0bf099f5296
Yannik Stradmann <Yannik.Stradmann@kip.uni-heidelberg.de>
2019-08-27 22:20:04 +0200

    Remove unnecessary import in dlens toplevel
    Change-Id: Ic31ec6c9e837c2831490b8cfa0955cb9a50cb5ca

--- dlens/dlens/__init__.py
+++ dlens/dlens/__init__.py
@@ -2,8 +2,3 @@ import pylogging as logger
 
 logger.reset()
 logger.default_config(level=logger.LogLevel.DEBUG)
-
-try:
-    import dlens.v2
-except ImportError:
-    logger.get("dlens").warn("Warning: v2 module not available")
