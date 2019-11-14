3604add2c667e5fbaa48d50f9db884df044ec77e
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2019-11-14 11:21:02 +0100

    Poll for completion in run_ppu_program
    Change-Id: Id5f1a31f62c15f6edea9a76c218da68e71b033c0

--- dlens/tools/vx/run_ppu_program.py
+++ dlens/tools/vx/run_ppu_program.py
@@ -1,80 +1,123 @@
 import ctypes
 import argparse
-from dlens_vx import hal, halco, sta
+from dlens_vx import halco
+from dlens_vx.sta import PlaybackProgramBuilder, PlaybackProgramExecutor, \
+    AutoConnection
+from pyhaldls_vx import PPUMemory, PPUControlRegister, Timer  # Issue 3429
 
 
 class PPUTimeoutError(Exception):
     pass
 
 
-def run_program(program_path: str,
-                as_string: bool = False,
-                wait: int = int(1E7)) -> int:
+def load_and_start_program(executor: PlaybackProgramExecutor,
+                           binary_path: str) -> None:
     """
-    :param program_path: Path to the PPU program binary
-    :param as_string: Interpret mailbox content as string
-    :param wait: Wait for n cycles before aborting the run
-    :return: Exit code from program execution
-    """
-    # Load the data
-    program = hal.PPUMemory()
-    program.load_from_file(program_path)
+    Load a PPU program and trigger its execution.
 
-    # PPU control register
-    ppu_control_reg_start = hal.PPUControlRegister()
+    :param executor: Connected executor to be used for loading and starting the
+                     program.
+    :param binary_path: Path to the program to be loaded
+    """
+    program = PPUMemory()
+    program.load_from_file(binary_path)
+
+    ppu_control_reg_start = PPUControlRegister()
     ppu_control_reg_start.inhibit_reset = True
 
-    ppu_control_reg_end = hal.PPUControlRegister()
+    ppu_control_reg_end = PPUControlRegister()
     ppu_control_reg_end.inhibit_reset = False
 
-    # Chip initialization
-    builder, _ = sta.generate(sta.InitGenerator())
-
-    # write, execute PPU program and read back results
+    builder = PlaybackProgramBuilder()
     builder.write(halco.PPUMemoryOnDLS(), program)
     builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_end)
     builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_start)
-    builder.write(halco.TimerOnDLS(), hal.Timer(0))
-    builder.wait_until(halco.TimerOnDLS(), wait)
-    status_handle = builder.read(halco.PPUStatusRegisterOnDLS())
+
+    executor.run(builder.done())
+
+
+def stop_program(executor: PlaybackProgramExecutor) -> int:
+    """
+    Stop the PPU, print the memory contents and evaluate the exit code.
+
+    :param executor: Connected executor to be used for stopping the program.
+    :return Exit code of the program
+    """
+    ppu_control_reg_end = PPUControlRegister()
+    ppu_control_reg_end.inhibit_reset = False
+
+    builder = PlaybackProgramBuilder()
     builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_end)
-    program_handle = builder.read(halco.PPUMemoryOnDLS())
+    return_handle = builder.read(
+        halco.PPUMemoryWordOnDLS(halco.PPUMemoryWordOnPPU.return_code,
+                                 halco.PPUOnDLS()))
     mailbox_handle = builder.read(
         halco.PPUMemoryBlockOnDLS(halco.PPUMemoryBlockOnPPU.mailbox))
 
     # Wait for all read responses to arrive
-    builder.write(halco.TimerOnDLS(), hal.Timer(0))
+    builder.write(halco.TimerOnDLS(), Timer(0))
     builder.wait_until(halco.TimerOnDLS(), 1000)
 
-    # Connect and execute
-    with sta.AutoConnection() as connection:
-        connection.run(builder.done())
+    # Run builder
+    executor.run(builder.done())
 
-    # Read mailbox
-    if as_string:
-        print(mailbox_handle.get().to_string())
-    else:
-        print(mailbox_handle.get())
+    # Print Mailbox
+    print(mailbox_handle.get().to_string())
 
-    # Check status register
-    if status_handle.get().sleep is not True:
-        raise PPUTimeoutError("PPU Program did not finish")
+    # Return the exit code
+    return ctypes.c_int32(int(return_handle.get().value)).value
 
-    # Extract exit code
-    raw_code = int(program_handle.get().words[
-        int(halco.PPUMemoryWordOnPPU.return_code)].value)
-    return ctypes.c_int32(raw_code).value
+
+def wait_until_ppu_finished(executor: PlaybackProgramExecutor,
+                            timeout: int = None) -> None:
+    """
+    Poll the PPU status register until program has finished. An optional
+    timeout may be specified.
+
+    :param executor: Connected executor to be used for polling the ppu status
+                     register
+    :param timeout: Timeout for the PPU program execution. Any number of
+                    FPGA cycles may be given, which correspond to the
+                    number of cycles the FPGA is actively waiting.
+                    The time needed for communication with the FPGA is
+                    neglected.
+    :raises PPUTimeoutError: PPU did not finish within max_polls
+    """
+    per_poll_wait = 1000
+    max_num_polls = timeout // per_poll_wait if timeout else None
+
+    poll_builder = PlaybackProgramBuilder()
+    status_handle = poll_builder.read(halco.PPUStatusRegisterOnDLS())
+    poll_builder.write(halco.TimerOnDLS(), Timer(0))
+    poll_builder.wait_until(halco.TimerOnDLS(), per_poll_wait)
+    poll_program = poll_builder.done()
+
+    executor.run(poll_program)
+    num_polls = 1
+    while status_handle.get().sleep is not True:
+        executor.run(poll_program)
+
+        if timeout is not None and num_polls > max_num_polls:
+            raise PPUTimeoutError("PPU execution did not finish in time.")
+
+        num_polls += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Execute PPU programs")
     parser.add_argument('program', type=str)
-    parser.add_argument('--as_string', action='store_true')
     parser.add_argument(
         '--wait', type=int, default=int(1E7),
-        help="Number of FPGA cycles after which the PPU is stopped")
+        help="Timeout for the PPU program execution. Any number of FPGA "
+             "cycles may be given, which correspond to the number of cycles "
+             "the FPGA is actively waiting. The time needed for communication "
+             "with the FPGA is neglected.")
     args = parser.parse_args()
 
-    code = run_program(args.program, args.as_string, args.wait)
-    if code != 0:
-        raise RuntimeError("PPU Program exited with exit code {}".format(code))
+    with AutoConnection() as connection:
+        load_and_start_program(connection, args.program)
+        wait_until_ppu_finished(connection, args.wait)
+        exit_code = stop_program(connection)
+
+    if exit_code != 0:
+        raise RuntimeError(f"PPU Program exited with exit code {exit_code}")
