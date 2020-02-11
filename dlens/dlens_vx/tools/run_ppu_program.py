c11058b65a37dc57adf5d6621c7903cb386bdcbd
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2020-02-11 17:43:35 +0000

    Make run_ppu_program's PPU adjustable
    Change-Id: I631b85f981a3eecfb168570a94d712b7c5bc9123

--- dlens/dlens_vx/tools/run_ppu_program.py
+++ dlens/dlens_vx/tools/run_ppu_program.py
@@ -7,19 +7,23 @@ from dlens_vx.sta import PlaybackProgramBuilder, PlaybackProgramExecutor, \
 from pyhaldls_vx import PPUControlRegister, Timer, PPUMemoryBlock  # Issue 3429
 from pylola_vx import PPUElfFile
 
+_DEFAULT_PPU = halco.PPUOnDLS(0)
+
 
 class PPUTimeoutError(Exception):
     pass
 
 
 def load_and_start_program(executor: PlaybackProgramExecutor,
-                           binary_path: str) -> None:
+                           binary_path: str,
+                           ppu: halco.PPUOnDLS = _DEFAULT_PPU) -> None:
     """
     Load a PPU program and trigger its execution.
 
     :param executor: Connected executor to be used for loading and starting the
                      program.
     :param binary_path: Path to the unstripped (*.bin) program to be loaded
+    :param ppu: PPU the program is started on.
     """
     builder = PlaybackProgramBuilder()
 
@@ -36,49 +40,48 @@ def load_and_start_program(executor: PlaybackProgramExecutor,
         halco.PPUMemoryWordOnPPU(program.size() - 1)
     )
 
-    program_on_dls = halco.PPUMemoryBlockOnDLS(program_on_ppu,
-                                               halco.PPUOnDLS())
+    program_on_dls = halco.PPUMemoryBlockOnDLS(program_on_ppu, ppu)
 
     # Ensure PPU is in reset state
-    builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_reset)
+    builder.write(ppu.toPPUControlRegisterOnDLS(), ppu_control_reg_reset)
 
     # Manually initialize memory where symbols will lie, issue #3477
     for _name, symbol in program_file.read_symbols().items():
         value = PPUMemoryBlock(symbol.coordinate.toPPUMemoryBlockSize())
-        symbol_on_dls = halco.PPUMemoryBlockOnDLS(symbol.coordinate,
-                                                  halco.PPUOnDLS())
+        symbol_on_dls = halco.PPUMemoryBlockOnDLS(symbol.coordinate, ppu)
         builder.write(symbol_on_dls, value)
 
     # Write PPU program
     builder.write(program_on_dls, program)
 
     # Set PPU to run state, start execution
-    builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_run)
+    builder.write(ppu.toPPUControlRegisterOnDLS(), ppu_control_reg_run)
     executor.run(builder.done())
 
 
 def stop_program(executor: PlaybackProgramExecutor,
-                 print_mailbox: bool = True) -> int:
+                 print_mailbox: bool = True,
+                 ppu: halco.PPUOnDLS = _DEFAULT_PPU) -> int:
     """
     Stop the PPU and evaluate the exit code. Optionally, read back the mailbox
     and print it.
 
     :param executor: Connected executor to be used for stopping the program.
     :param print_mailbox: Read back and print the mailbox as string to stdout.
+    :param ppu: PPU the program is stopped on.
     :return Exit code of the program
     """
     ppu_control_reg_end = PPUControlRegister()
     ppu_control_reg_end.inhibit_reset = False
 
     builder = PlaybackProgramBuilder()
-    builder.write(halco.PPUControlRegisterOnDLS(), ppu_control_reg_end)
+    builder.write(ppu.toPPUControlRegisterOnDLS(), ppu_control_reg_end)
     return_handle = builder.read(
-        halco.PPUMemoryWordOnDLS(halco.PPUMemoryWordOnPPU.return_code,
-                                 halco.PPUOnDLS()))
+        halco.PPUMemoryWordOnDLS(halco.PPUMemoryWordOnPPU.return_code, ppu))
 
     if print_mailbox:
         mailbox_handle = builder.read(
-            halco.PPUMemoryBlockOnDLS(halco.PPUMemoryBlockOnPPU.mailbox))
+            halco.PPUMemoryBlockOnDLS(halco.PPUMemoryBlockOnPPU.mailbox, ppu))
     else:
         mailbox_handle = None
 
@@ -98,7 +101,8 @@ def stop_program(executor: PlaybackProgramExecutor,
 
 
 def wait_until_ppu_finished(executor: PlaybackProgramExecutor,
-                            timeout: int = None) -> None:
+                            timeout: int = None,
+                            ppu: halco.PPUOnDLS = _DEFAULT_PPU) -> None:
     """
     Poll the PPU status register until program has finished. An optional
     timeout may be specified.
@@ -110,13 +114,14 @@ def wait_until_ppu_finished(executor: PlaybackProgramExecutor,
                     number of cycles the FPGA is actively waiting.
                     The time needed for communication with the FPGA is
                     neglected.
+    :param ppu: PPU whose completion is awaited.
     :raises PPUTimeoutError: PPU did not finish within max_polls
     """
     per_poll_wait = 1000
     max_num_polls = timeout // per_poll_wait if timeout else None
 
     poll_builder = PlaybackProgramBuilder()
-    status_handle = poll_builder.read(halco.PPUStatusRegisterOnDLS())
+    status_handle = poll_builder.read(ppu.toPPUStatusRegisterOnDLS())
     poll_builder.write(halco.TimerOnDLS(), Timer(0))
     poll_builder.wait_until(halco.TimerOnDLS(), per_poll_wait)
     poll_program = poll_builder.done()
@@ -141,15 +146,19 @@ if __name__ == "__main__":
              "cycles may be given, which correspond to the number of cycles "
              "the FPGA is actively waiting. The time needed for communication "
              "with the FPGA is neglected.")
+    parser.add_argument('--ppu_id', type=int, default=_DEFAULT_PPU.toEnum(),
+                        help="PPU ID used for program execution.")
     args = parser.parse_args()
 
     with AutoConnection() as connection:
         init_builder, _ = generate(DigitalInit())
         connection.run(init_builder.done())
 
-        load_and_start_program(connection, args.program)
-        wait_until_ppu_finished(connection, args.wait)
-        exit_code = stop_program(connection)
+        load_and_start_program(connection, args.program,
+                               halco.PPUOnDLS(args.ppu_id))
+        wait_until_ppu_finished(connection, args.wait,
+                                halco.PPUOnDLS(args.ppu_id))
+        exit_code = stop_program(connection, ppu=halco.PPUOnDLS(args.ppu_id))
 
     if exit_code != 0:
         raise RuntimeError(f"PPU Program exited with exit code {exit_code}")
