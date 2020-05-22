718604ad44441f9af6d86d89d2169e600db2a403
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2020-05-22 15:42:12 +0200

    Migrate synapse driver config to HXv2
    Depends-On: 11971
    Change-Id: Idd95c9678faf07c507cc0e2f739dd28925b304b5

--- include/haldls/vx/v2/synapse_driver.h
+++ include/haldls/vx/v2/synapse_driver.h
@@ -1,10 +1,285 @@
 #pragma once
+#include "halco/hicann-dls/vx/v2/synapse_driver.h"
 #include "haldls/vx/genpybind.h"
 #include "haldls/vx/synapse_driver.h"
 
+namespace cereal {
+class access;
+} // namespace cereal
+
+namespace fisch::vx {
+class OmnibusChipOverJTAG;
+class Omnibus;
+} // namespace fisch::vx
+
 namespace haldls::vx::v2 GENPYBIND_TAG_HALDLS_VX_V2 {
 
-using SynapseDriverConfig GENPYBIND(visible) = haldls::vx::SynapseDriverConfig;
 using SynapseDriverSRAMTimingConfig GENPYBIND(visible) = haldls::vx::SynapseDriverSRAMTimingConfig;
 
+class GENPYBIND(visible) SynapseDriverConfig : public DifferentialWriteTrait
+{
+public:
+	typedef halco::hicann_dls::vx::SynapseDriverOnDLS coordinate_type;
+	typedef std::true_type is_leaf_node;
+	constexpr static auto unsupported_read_targets GENPYBIND(hidden) = {
+	    hxcomm::vx::Target::simulation, hxcomm::vx::Target::hardware};
+
+	struct GENPYBIND(inline_base("*")) Utilization
+	    : public halco::common::detail::RantWrapper<Utilization, uint_fast8_t, 15, 0>
+	{
+		constexpr explicit Utilization(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	struct GENPYBIND(inline_base("*")) Offset
+	    : public halco::common::detail::RantWrapper<Offset, uint_fast8_t, 15, 0>
+	{
+		constexpr explicit Offset(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	struct GENPYBIND(inline_base("*")) Recovery
+	    : public halco::common::detail::RantWrapper<Recovery, uint_fast8_t, 15, 0>
+	{
+		constexpr explicit Recovery(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	struct GENPYBIND(inline_base("*")) HagenDACOffset
+	    : public halco::common::detail::RantWrapper<HagenDACOffset, uint_fast8_t, 63, 0>
+	{
+		constexpr explicit HagenDACOffset(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	struct GENPYBIND(inline_base("*")) TargetVoltages
+	    : public halco::common::detail::RantWrapper<TargetVoltages, uint_fast8_t, 1, 0>
+	{
+		constexpr explicit TargetVoltages(uintmax_t const val = 0) GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	struct GENPYBIND(inline_base("*")) RowAddressCompareMask
+	    : public halco::common::detail::RantWrapper<RowAddressCompareMask, uint_fast8_t, 31, 0>
+	{
+		constexpr explicit RowAddressCompareMask(uintmax_t const val = 0b11111)
+		    GENPYBIND(implicit_conversion) :
+		    rant_t(val)
+		{}
+	};
+
+	enum class GENPYBIND(visible) RowMode
+	{
+		disabled,
+		excitatory,
+		inhibitory,
+		excitatory_and_inhibitory
+	};
+
+	SynapseDriverConfig() SYMBOL_VISIBLE;
+
+	// accessors
+
+	/** Enable padi bus receiver. */
+	GENPYBIND(getter_for(enable_receiver))
+	bool get_enable_receiver() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_receiver))
+	void set_enable_receiver(bool const value) SYMBOL_VISIBLE;
+
+	/**
+	 * Set row select address compare mask.
+	 * The incoming row select address allows to individually address a single
+	 * synapse driver. For this purpose, the events row select address (5 bit)
+	 * is compared to the synapse driver index (static).
+	 * The padi bus receiver can be configured to ignore (parts of) the row
+	 * select address by zeroing (parts of) the `row_address_compare_mask`.
+	 */
+	GENPYBIND(getter_for(row_address_compare_mask))
+	RowAddressCompareMask get_row_address_compare_mask() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(row_address_compare_mask))
+	void set_row_address_compare_mask(RowAddressCompareMask const value) SYMBOL_VISIBLE;
+
+	/**
+	 * This enables sending the five lower bits of an address to the synapses.
+	 * The MSB is always sent. This mode must be disabled for HAGEN operation.
+	 */
+	GENPYBIND(getter_for(enable_address_out))
+	bool get_enable_address_out() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_address_out))
+	void set_enable_address_out(bool const value) SYMBOL_VISIBLE;
+
+	/** Set strength of STP utilization. */
+	GENPYBIND(getter_for(utilization))
+	Utilization get_utilization() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(utilization))
+	void set_utilization(Utilization const value) SYMBOL_VISIBLE;
+
+	/** Calibration value for dacen pulse width. */
+	GENPYBIND(getter_for(offset))
+	Offset get_offset() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(offset))
+	void set_offset(Offset const value) SYMBOL_VISIBLE;
+
+	/** Enable the DAC converting a hagen activation to an STP voltage. */
+	GENPYBIND(getter_for(enable_hagen_dac))
+	bool get_enable_hagen_dac() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_hagen_dac))
+	void set_enable_hagen_dac(bool value) SYMBOL_VISIBLE;
+
+	/** Set STP recovery time constant. */
+	GENPYBIND(getter_for(recovery))
+	Recovery get_recovery() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(recovery))
+	void set_recovery(Recovery const value) SYMBOL_VISIBLE;
+
+	/** Set bottom row mode. */
+	GENPYBIND(getter_for(row_mode_bottom))
+	RowMode get_row_mode_bottom() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(row_mode_bottom))
+	void set_row_mode_bottom(RowMode value) SYMBOL_VISIBLE;
+
+	/** Set top row mode. */
+	GENPYBIND(getter_for(row_mode_top))
+	RowMode get_row_mode_top() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(row_mode_top))
+	void set_row_mode_top(RowMode value) SYMBOL_VISIBLE;
+
+	/**
+	 * Output voltage offset for the hagen mode DAC.
+	 * While the STP offsets also affect hagen usage, this setting allows calibration of dacen
+	 * pulse timings in hagen mode across a larger range.
+	 */
+	GENPYBIND(getter_for(hagen_dac_offset))
+	HagenDACOffset get_hagen_dac_offset() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(hagen_dac_offset))
+	void set_hagen_dac_offset(HagenDACOffset value) SYMBOL_VISIBLE;
+
+	/**
+	 * Select a pair of STP target voltages (vrecover, vcharge).
+	 * Each synapse driver is provided with two sets of the STP target
+	 * voltages, to allow for two different parameterizations within a single
+	 * array.
+	 * Note that although there are only two logical arrays of synapse drivers
+	 * per chip, even and odd synapse drivers are fed with disjunct analog
+	 * parameters provided by the east and west capmem instances, respectively.
+	 */
+	GENPYBIND(getter_for(select_target_voltages))
+	TargetVoltages get_select_target_voltages() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(select_target_voltages))
+	void set_select_target_voltages(TargetVoltages const value) SYMBOL_VISIBLE;
+
+	/**
+	 * Enable readout source follower and connect to readout line.
+	 * The readout is shared between all instances of a vertical half.
+	 * Connecting multiple instances is non-destructive but will result
+	 * in faulty readings.
+	 * Also note that an additional buffer per half must be biased to drive the
+	 * signals further down to the readout chain.
+	 */
+	GENPYBIND(getter_for(enable_readout))
+	bool get_enable_readout() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_readout))
+	void set_enable_readout(bool const value) SYMBOL_VISIBLE;
+
+	/** Enable renewing synapses for LIF sampling (broken due to faulty circuit). */
+	GENPYBIND(getter_for(enable_renewing))
+	bool get_enable_renewing() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_renewing))
+	void set_enable_renewing(bool const value) SYMBOL_VISIBLE;
+
+	/**
+	 * Enable modulation of pulse width proportional to event address.
+	 * Note:
+	 *   - Hagen DAC must also be enabled through `enable_hagen_dac`
+	 *   - Address forwarding must be disabled through `enable_address_out`
+	 *   - STP circuit must be enabled through `enable_stp`
+	 */
+	GENPYBIND(getter_for(enable_hagen_modulation))
+	bool get_enable_hagen_modulation() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_hagen_modulation))
+	void set_enable_hagen_modulation(bool const value) SYMBOL_VISIBLE;
+
+	/** Enable STP circuit. */
+	GENPYBIND(getter_for(enable_stp))
+	bool get_enable_stp() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_stp))
+	void set_enable_stp(bool const value) SYMBOL_VISIBLE;
+
+	/** Enable charge sharing for larger STP utilization values (broken). */
+	GENPYBIND(getter_for(enable_charge_sharing))
+	bool get_enable_charge_sharing() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_charge_sharing))
+	void set_enable_charge_sharing(bool const value) SYMBOL_VISIBLE;
+
+	/** Enable STP recovery circuit. */
+	GENPYBIND(getter_for(enable_recovery))
+	bool get_enable_recovery() const SYMBOL_VISIBLE;
+	GENPYBIND(setter_for(enable_recovery))
+	void set_enable_recovery(bool const value) SYMBOL_VISIBLE;
+
+	bool operator==(SynapseDriverConfig const& other) const SYMBOL_VISIBLE;
+	bool operator!=(SynapseDriverConfig const& other) const SYMBOL_VISIBLE;
+
+	static size_t constexpr config_size_in_words GENPYBIND(hidden) = 3;
+	template <typename AddressT>
+	static std::array<AddressT, config_size_in_words> addresses(
+	    coordinate_type const& synapse_driver) SYMBOL_VISIBLE GENPYBIND(hidden);
+	template <typename WordT>
+	std::array<WordT, config_size_in_words> encode() const SYMBOL_VISIBLE GENPYBIND(hidden);
+	template <typename WordT>
+	void decode(std::array<WordT, config_size_in_words> const& data) SYMBOL_VISIBLE
+	    GENPYBIND(hidden);
+
+	GENPYBIND(stringstream)
+	friend std::ostream& operator<<(std::ostream& os, SynapseDriverConfig const& config)
+	    SYMBOL_VISIBLE;
+
+private:
+	friend class cereal::access;
+	template <class Archive>
+	void serialize(Archive& ar, std::uint32_t const version) SYMBOL_VISIBLE;
+
+	struct SynapseDriverConfigBitfield;
+
+	SynapseDriverConfigBitfield to_bitfield() const;
+	void from_bitfield(SynapseDriverConfigBitfield const& bitfield);
+
+	bool m_en_receiver;
+	RowAddressCompareMask m_row_address_compare_mask;
+	bool m_en_address_out;
+	Utilization m_utilization;
+	Offset m_offset;
+	bool m_en_hagen_dac;
+	Recovery m_recovery;
+	RowMode m_row_mode_bottom;
+	RowMode m_row_mode_top;
+	HagenDACOffset m_hagen_dac_offset;
+	TargetVoltages m_select_target_voltages;
+	bool m_en_readout;
+	bool m_en_renewing;
+	bool m_en_hagen_modulation;
+	bool m_en_stp;
+	bool m_en_charge_sharing;
+	bool m_en_recovery;
+};
+
+std::ostream& operator<<(std::ostream& os, SynapseDriverConfig::RowMode const& mode) SYMBOL_VISIBLE;
+
 } // namespace haldls::vx::v2
+
+namespace haldls::vx::detail {
+
+template <>
+struct BackendContainerTrait<v2::SynapseDriverConfig>
+    : public BackendContainerBase<
+          v2::SynapseDriverConfig,
+          fisch::vx::Omnibus,
+          fisch::vx::OmnibusChipOverJTAG>
+{};
+
+} // namespace haldls::vx::detail
---
+++ tests/sw/haldls/vx/v1/test-synapse_driver.cpp
@@ -0,0 +1,179 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+#include "fisch/vx/jtag.h"
+#include "halco/common/typed_array.h"
+#include "halco/hicann-dls/vx/v1/coordinates.h"
+#include "haldls/vx/common.h"
+#include "haldls/vx/omnibus_constants.h"
+#include "haldls/vx/v1/synapse_driver.h"
+#include "test-helper.h"
+
+using namespace fisch::vx;
+using namespace haldls::vx::v1;
+using namespace halco::hicann_dls::vx::v1;
+using namespace halco::common;
+
+typedef std::vector<halco::hicann_dls::vx::OmnibusChipOverJTAGAddress> addresses_type;
+typedef std::vector<fisch::vx::OmnibusChipOverJTAG> words_type;
+
+TEST(SynapseDriverConfig, General)
+{
+	SynapseDriverConfig config;
+
+	bool value;
+
+	value = !config.get_enable_receiver();
+	config.set_enable_receiver(value);
+	ASSERT_EQ(config.get_enable_receiver(), value);
+
+	value = !config.get_enable_address_out();
+	config.set_enable_address_out(value);
+	ASSERT_EQ(config.get_enable_address_out(), value);
+
+	{
+		auto const value = SynapseDriverConfig::RowMode::excitatory;
+		config.set_row_mode_bottom(value);
+		ASSERT_EQ(config.get_row_mode_bottom(), value);
+	}
+
+	{
+		auto const value = SynapseDriverConfig::RowMode::inhibitory;
+		config.set_row_mode_top(value);
+		ASSERT_EQ(config.get_row_mode_top(), value);
+	}
+
+	value = !config.get_enable_readout();
+	config.set_enable_readout(value);
+	ASSERT_EQ(config.get_enable_readout(), value);
+
+	value = !config.get_enable_renewing();
+	config.set_enable_renewing(value);
+	ASSERT_EQ(config.get_enable_renewing(), value);
+
+	value = !config.get_enable_hagen_modulation();
+	config.set_enable_hagen_modulation(value);
+	ASSERT_EQ(config.get_enable_hagen_modulation(), value);
+
+	value = !config.get_enable_stp();
+	config.set_enable_stp(value);
+	ASSERT_EQ(config.get_enable_stp(), value);
+
+	value = !config.get_enable_charge_sharing();
+	config.set_enable_charge_sharing(value);
+	ASSERT_EQ(config.get_enable_charge_sharing(), value);
+
+	value = !config.get_enable_recovery();
+	config.set_enable_recovery(value);
+	ASSERT_EQ(config.get_enable_recovery(), value);
+
+	auto row_address_compare_mask =
+	    draw_ranged_non_default_value<SynapseDriverConfig::RowAddressCompareMask>(
+	        config.get_row_address_compare_mask());
+	config.set_row_address_compare_mask(row_address_compare_mask);
+	ASSERT_EQ(config.get_row_address_compare_mask(), row_address_compare_mask);
+
+	auto utilization = SynapseDriverConfig::Utilization(config.get_utilization() + 1);
+	config.set_utilization(utilization);
+	ASSERT_EQ(config.get_utilization(), utilization);
+
+	auto offset = SynapseDriverConfig::Offset(config.get_offset() + 1);
+	config.set_offset(offset);
+	ASSERT_EQ(config.get_offset(), offset);
+
+	auto recovery = SynapseDriverConfig::Recovery(config.get_recovery() + 1);
+	config.set_recovery(recovery);
+	ASSERT_EQ(config.get_recovery(), recovery);
+
+	auto select_target_voltages =
+	    SynapseDriverConfig::TargetVoltages(config.get_select_target_voltages() + 1);
+	config.set_select_target_voltages(select_target_voltages);
+	ASSERT_EQ(config.get_select_target_voltages(), select_target_voltages);
+
+	SynapseDriverConfig default_config;
+	ASSERT_NE(config, default_config);
+	ASSERT_TRUE(config != default_config);
+	ASSERT_FALSE(config == default_config);
+	default_config = config;
+	ASSERT_EQ(config, default_config);
+	ASSERT_TRUE(config == default_config);
+	ASSERT_FALSE(config != default_config);
+}
+
+TEST(SynapseDriverConfig, EncodeDecode)
+{
+	SynapseDriverConfig config;
+	config.set_recovery(SynapseDriverConfig::Recovery(7));
+	config.set_enable_receiver(true);
+	config.set_row_mode_top(SynapseDriverConfig::RowMode::excitatory);
+	config.set_enable_readout(true);
+
+	auto coord =
+	    SynapseDriverOnDLS(SynapseDriverOnSynapseDriverBlock(42), SynapseDriverBlockOnDLS(1));
+
+	std::array<OmnibusChipOverJTAGAddress, SynapseDriverConfig::config_size_in_words>
+	    ref_addresses = {OmnibusChipOverJTAGAddress{0x118400 + 42 * 4 + 0},
+	                     OmnibusChipOverJTAGAddress{0x118400 + 42 * 4 + 1},
+	                     OmnibusChipOverJTAGAddress{0x118400 + 42 * 4 + 2}};
+
+	{ // check if write addresses are correct
+		addresses_type write_addresses;
+		visit_preorder(config, coord, stadls::WriteAddressVisitor<addresses_type>{write_addresses});
+		EXPECT_THAT(write_addresses, ::testing::ElementsAreArray(ref_addresses));
+	}
+
+	{ // check if read addresses are correct
+		addresses_type read_addresses;
+		visit_preorder(config, coord, stadls::ReadAddressVisitor<addresses_type>{read_addresses});
+		EXPECT_THAT(read_addresses, ::testing::ElementsAreArray(ref_addresses));
+	}
+
+	// Encode
+	words_type data;
+	visit_preorder(config, coord, stadls::EncodeVisitor<words_type>{data});
+	ASSERT_EQ(data[1].get() & 0b00001111, 14);       // recovery (bits reversed)
+	ASSERT_TRUE(data[0].get() & 0b0000000100000000); // en_receiver
+	ASSERT_TRUE(data[1].get() & 0b0000000000100000); // en_exc_top
+	ASSERT_TRUE(data[2].get() & 0b0000000000000010); // en_readout
+
+	// Decode back
+	SynapseDriverConfig config_copy;
+	ASSERT_NE(config, config_copy);
+	visit_preorder(config_copy, coord, stadls::DecodeVisitor<words_type>{std::move(data)});
+	ASSERT_EQ(config, config_copy);
+}
+
+TEST(SynapseDriverConfig, CerealizeCoverage)
+{
+	SynapseDriverConfig c1, c2;
+	c1.set_recovery(SynapseDriverConfig::Recovery(7));
+	c1.set_row_mode_top(SynapseDriverConfig::RowMode::excitatory);
+	c1.set_row_mode_bottom(SynapseDriverConfig::RowMode::inhibitory);
+	c1.set_enable_receiver(true);
+	c1.set_row_address_compare_mask(SynapseDriverConfig::RowAddressCompareMask(0b01010));
+	c1.set_enable_address_out(false);
+	c1.set_utilization(SynapseDriverConfig::Utilization(3));
+	c1.set_offset(SynapseDriverConfig::Offset(4));
+	c1.set_recovery(SynapseDriverConfig::Recovery(5));
+	c1.set_select_target_voltages(SynapseDriverConfig::TargetVoltages(1));
+	c1.set_enable_readout(true);
+	c1.set_enable_renewing(true);
+	c1.set_enable_hagen_modulation(true);
+	c1.set_enable_stp(true);
+	c1.set_enable_charge_sharing(true);
+	c1.set_enable_recovery(true);
+
+	std::ostringstream ostream;
+	{
+		cereal::JSONOutputArchive oa(ostream);
+		oa(c1);
+	}
+
+	std::istringstream istream(ostream.str());
+	{
+		cereal::JSONInputArchive ia(istream);
+		ia(c2);
+	}
+	ASSERT_EQ(c1, c2);
+}
