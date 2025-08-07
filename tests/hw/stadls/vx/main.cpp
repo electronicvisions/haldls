ddf3c952af74183d6768f98efbe726bf55889787
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2025-08-07 16:10:33 +0200

    Default hxcomm loglevel to WARN during test execution
    Change-Id: I427ae8a44c657cfbef92767a3b4d43f8d762bf53

--- tests/hw/stadls/vx/main.cpp
+++ tests/hw/stadls/vx/main.cpp
@@ -22,28 +22,34 @@ int main(int argc, char* argv[])
 	std::string loglevel;
 	namespace bpo = boost::program_options;
 	bpo::options_description desc("Options");
-	desc.add_options()("loglevel", bpo::value<std::string>(&loglevel)->default_value("info"));
+	desc.add_options()("loglevel", bpo::value<std::string>(&loglevel));
 
 	bpo::variables_map vm;
 	bpo::store(
 	    bpo::basic_command_line_parser(argc, argv).options(desc).allow_unregistered().run(), vm);
 	bpo::notify(vm);
 
-	if (loglevel == "trace") {
-		logger_default_config(log4cxx::Level::getTrace());
-	} else if (loglevel == "debug") {
-		logger_default_config(log4cxx::Level::getDebug());
-	} else if (loglevel == "info") {
+	if (!vm.count("loglevel")) {
+		// defaults, if not overridden by command line arguments
 		logger_default_config(log4cxx::Level::getInfo());
-	} else if (loglevel == "warning") {
-		logger_default_config(log4cxx::Level::getWarn());
-	} else if (loglevel == "error") {
-		logger_default_config(log4cxx::Level::getError());
-	} else if (loglevel == "fatal") {
-		logger_default_config(log4cxx::Level::getFatal());
+		logger_set_loglevel(log4cxx::Logger::getLogger("hxcomm"), log4cxx::Level::getWarn());
 	} else {
-		throw std::runtime_error(
-		    "loglevel option has to be one of {trace,debug,info,warning,error,fatal}");
+		if (loglevel == "trace") {
+			logger_default_config(log4cxx::Level::getTrace());
+		} else if (loglevel == "debug") {
+			logger_default_config(log4cxx::Level::getDebug());
+		} else if (loglevel == "info") {
+			logger_default_config(log4cxx::Level::getInfo());
+		} else if (loglevel == "warning") {
+			logger_default_config(log4cxx::Level::getWarn());
+		} else if (loglevel == "error") {
+			logger_default_config(log4cxx::Level::getError());
+		} else if (loglevel == "fatal") {
+			logger_default_config(log4cxx::Level::getFatal());
+		} else {
+			throw std::runtime_error(
+			    "loglevel option has to be one of {trace,debug,info,warning,error,fatal}");
+		}
 	}
 
 	return RUN_ALL_TESTS();
