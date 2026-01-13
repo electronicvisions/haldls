6761904ec797005325198f33bac19666726e0e54
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2026-01-13 13:20:00 +0100

    fix: Decode random/ones sw test
    * Checking that the decoding doesn't throw instead of that the result is
      unequal to default-constructing the container is safe for all random
      sequences and also for one-ing containers which are by default already
      one-ed.
    
    Change-Id: I1a88043cf6c3294077f28c877df4dedc0eb494b5

--- tests/sw/stadls/vx/v3/test-decode.cpp
+++ tests/sw/stadls/vx/v3/test-decode.cpp
@@ -9,46 +9,36 @@
 #include <random>
 
 typedef hate::type_list<
-    haldls::vx::NullPayloadReadable,
     haldls::vx::INA219Status,
+    haldls::vx::NullPayloadReadable,
     haldls::vx::TMP112Status,
-    haldls::vx::VectorGeneratorControl,
+    lola::vx::v3::AtomicNeuron,
+    lola::vx::v3::Chip,
     lola::vx::v3::LogicalNeuron>
     NotRandomizableTypes;
 
 typedef hate::type_list<
-    lola::vx::v3::AtomicNeuron,
-    lola::vx::v3::Chip,
-    lola::vx::v3::LogicalNeuron,
-    haldls::vx::SynapseBiasSelection,
-    lola::vx::v3::CapMem,
     haldls::vx::NullPayloadReadable,
-    haldls::vx::CommonPhyConfigChip,
-    haldls::vx::CommonPhyConfigFPGA,
-    haldls::vx::v3::CapMemBlockConfig,
     haldls::vx::INA219Status,
-    haldls::vx::SpikeIOInputRoute,             // One'd by default
-    haldls::vx::SpikeIOOutputRoute,            // One'd by default
-    haldls::vx::TCA9546ChannelConfig,          // One'd by default
-    haldls::vx::CADCOffsetSRAMTimingConfig,    // One'd by default
-    haldls::vx::NeuronSRAMTimingConfig,        // One'd by default
-    haldls::vx::NeuronBackendSRAMTimingConfig, // One'd by default
-    haldls::vx::VectorGeneratorControl>
+    haldls::vx::v3::CapMemBlockConfig,
+    lola::vx::v3::AtomicNeuron,
+    lola::vx::v3::CapMem,
+    lola::vx::v3::Chip,
+    lola::vx::v3::LogicalNeuron>
     NotOneableTypes;
 
 #define PLAYBACK_CONTAINER(Name, Type)                                                             \
 	TEST(Name, IsRandomizable)                                                                     \
 	{                                                                                              \
 		if constexpr (!hate::is_in_type_list<Type, NotRandomizableTypes>::value) {                 \
-			std::mt19937 rng(212345);                                                              \
+			std::mt19937 rng(std::random_device{}());                                              \
 			Type config;                                                                           \
-			stadls::vx::decode_random<Type>(rng, config);                                          \
 			typedef typename haldls::vx::detail::BackendContainerTrait<Type>::default_container    \
 			    word_type;                                                                         \
 			if (haldls::vx::detail::IsReadable<Type>::value &&                                     \
-			    fisch::vx::IsReadable<decltype(                                                    \
-			        fisch::vx::container_cast(std::declval<word_type>()))>::value) {               \
-				EXPECT_NE(config, Type());                                                         \
+			    fisch::vx::IsReadable<decltype(fisch::vx::container_cast(                          \
+			        std::declval<word_type>()))>::value) {                                         \
+				EXPECT_NO_THROW((stadls::vx::decode_random<Type>(rng, config)));                   \
 			}                                                                                      \
 		}                                                                                          \
 	}                                                                                              \
@@ -57,13 +47,12 @@ typedef hate::type_list<
 	{                                                                                              \
 		if constexpr (!hate::is_in_type_list<Type, NotOneableTypes>::value) {                      \
 			Type config;                                                                           \
-			stadls::vx::decode_ones<Type>(config);                                                 \
 			typedef typename haldls::vx::detail::BackendContainerTrait<Type>::default_container    \
 			    word_type;                                                                         \
 			if (haldls::vx::detail::IsReadable<Type>::value &&                                     \
-			    fisch::vx::IsReadable<decltype(                                                    \
-			        fisch::vx::container_cast(std::declval<word_type>()))>::value) {               \
-				EXPECT_NE(config, Type());                                                         \
+			    fisch::vx::IsReadable<decltype(fisch::vx::container_cast(                          \
+			        std::declval<word_type>()))>::value) {                                         \
+				EXPECT_NO_THROW((stadls::vx::decode_ones<Type>(config)));                          \
 			}                                                                                      \
 		}                                                                                          \
 	}
